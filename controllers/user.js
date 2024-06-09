f96ef658d2d9f86031d58139d44ce225fce04760
Abhishek kumawat <abhikumardambiwal@gmail.com>
2024-06-10 00:09:47 +0530

    c

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/features.js";
-
+import ErrorHandler from "../middlewares/error.js";
 export const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
